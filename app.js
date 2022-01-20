5f0d1b82071cb129e2f83893e38df16c4a1769fa
Elliott Parker <kparker1848@gmail.com>
2022-01-19 20:00:11 -0600

    styling, added custom fonts, bg, sweetalert and animista

--- app.js
+++ app.js
@@ -2,21 +2,25 @@ $(document).ready(function () {
 
     function makeAccusation() {
         for (var i = 0; i <= 100; i++) {
-            var friends = ["Kelsie", "Kelsey", "Paxton", "JT", "Aaron"];
-            var locations = ["bedroom", "living room", "porch", "party room", "library", "kitchen", "solarium", "dining room", "studio", "game room"]
-            var objects = ["banana taped to a stick", "giant wheel of cheese", "arsenic", "rock", "knife", "sword", "silver bullet", "holy water",
+            var friends = ["Resetti", "Brewster", "Sable", "Tom", "Isabelle"];
+            var locations = ["beach shack", "town hall", "sewing shop", "boat shed", "library", "kitchen", "solarium", "dining room", "studio", "game room"]
+            var objects = ["banana taped to a stick", "giant wheel of cheese", "arsenic", "rock", "net", "shovel", "silver bullet", "holy water",
                 "cursed necklace", "cow plant", "acid", "really bad singing", "giant mecha robot", "stake", "glass bottle", "demon portal", "rattlesnake",
-                "gun", "mysterious liquid", "laser turrets"];
+                "slingshot", "mysterious liquid", "laser turrets"];
             var h3 = $("<h3></h3>");
 
             h3.text("Accusation " + [i + 1]);
-            $("body").append(h3);
+            $(".acc-box").append(h3);
             h3.click(alertAccusation(i));
         };
 
         function alertAccusation(i) {
             return function () {
-                alert(`I accuse ${friends[i % friends.length]}, with the ${objects[i % objects.length]} in the ${locations[i % locations.length]}!`);
+                swal({
+                    title: `I accuse ${friends[i % friends.length]},`,
+                    text: `with the ${objects[i % objects.length]} in the ${locations[i % locations.length]}!`,
+                    button: "Case Closed",
+                  });
             };
         };
 
