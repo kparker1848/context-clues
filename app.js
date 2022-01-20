9bb24a8f8889e80a990f486b368b35e81a2630c5
Elliott Parker <kparker1848@gmail.com>
2022-01-20 00:12:56 -0600

    changed styling and added a random accusation generation mode

--- app.js
+++ app.js
@@ -1,17 +1,75 @@
-$(document).ready(function () { 
+$(document).ready(function () {
+
+    var accButton = $("<button></button>");
+    accButton.addClass("acc-button");
+    accButton.text("Make Accusation!");
+    $(".btn-box").append(accButton);
+
+    var accTextBox = $("<h3></h3>");
+    accTextBox.addClass("make-acc-text");
+
+    var allAccButton = $("<button></button>");
+    allAccButton.addClass("acc-button");
+    allAccButton.text("Show all Accusations!");
+    $(".btn-box").append(allAccButton);
+
+
+    accButton.click(makeAccusation);
+
+    allAccButton.click(showAccusations);
+
+    allAccButton.dblclick(clearAccusations);
+
+
 
     function makeAccusation() {
+
+        clearAccusations();
+
         for (var i = 0; i <= 100; i++) {
             var friends = ["Resetti", "Brewster", "Sable", "Tom", "Isabelle"];
             var locations = ["beach shack", "town hall", "sewing shop", "boat shed", "library", "kitchen", "solarium", "dining room", "studio", "game room"]
             var objects = ["banana taped to a stick", "giant wheel of cheese", "arsenic", "rock", "net", "shovel", "silver bullet", "holy water",
                 "cursed necklace", "cow plant", "acid", "really bad singing", "giant mecha robot", "stake", "glass bottle", "demon portal", "rattlesnake",
                 "slingshot", "mysterious liquid", "laser turrets"];
-            var h3 = $("<h3></h3>");
+        };
 
-            h3.text("Accusation " + [i + 1]);
-            $(".acc-box").append(h3);
-            h3.click(alertAccusation(i));
+        var i = Math.floor((Math.random() * 100) + 1);
+
+        accTextBox.text("");
+        accTextBox.text("Accusation " + [i]);
+        $(".acc-box").append(accTextBox);
+        accTextBox.click(alertAccusation(i));
+
+        function alertAccusation(i) {
+
+            return function () {
+                swal({
+                    title: `I accuse ${friends[i % friends.length]},`,
+                    text: `with the ${objects[i % objects.length]} in the ${locations[i % locations.length]}!`,
+                    button: "Case Closed",
+                });
+            };
+        };
+
+    };
+
+    function showAccusations() {
+
+        clearAccusations();
+
+        for (var i = 0; i <= 100; i++) {
+            var friends = ["Resetti", "Brewster", "Sable", "Tom", "Isabelle"];
+            var locations = ["beach shack", "town hall", "sewing shop", "boat shed", "library", "kitchen", "solarium", "dining room", "studio", "game room"]
+            var objects = ["banana taped to a stick", "giant wheel of cheese", "arsenic", "rock", "net", "shovel", "silver bullet", "holy water",
+                "cursed necklace", "cow plant", "acid", "really bad singing", "giant mecha robot", "stake", "glass bottle", "demon portal", "rattlesnake",
+                "slingshot", "mysterious liquid", "laser turrets"];
+            var allAccText = $("<h3></h3>");
+            allAccText.addClass("show-all-text");
+
+            allAccText.text("Accusation " + [i + 1]);
+            $(".acc-box").append(allAccText);
+            allAccText.click(alertAccusation(i));
         };
 
         function alertAccusation(i) {
@@ -20,11 +78,14 @@ $(document).ready(function () {
                     title: `I accuse ${friends[i % friends.length]},`,
                     text: `with the ${objects[i % objects.length]} in the ${locations[i % locations.length]}!`,
                     button: "Case Closed",
-                  });
+                });
             };
         };
-
     };
 
-    makeAccusation();
+    function clearAccusations() {
+
+        $(".acc-box").empty(".show-all-text");
+    };
+
 });
